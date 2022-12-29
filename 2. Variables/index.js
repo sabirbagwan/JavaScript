e3b9f131dd466681134f0cbc1c0f6bb7aff9a88a
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2022-12-29 11:30:51 +0530

    Add files via upload

---
+++ 2. Variables/index.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+console.log("I love Biryani");
+
+// window.alert("I really love Biryani");
+
+let firstname = "Wolverine" // Strings
+let age; 
+age = 31; // number 
+let student = false; // boolean
+let age1 = 32;
+
+age = age + 1;
+
+console.log("hello",firstname)
+console.log(age);
+console.log(age1);
+console.log(student)
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+document.getElementById("p2").innerHTML = "you are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + stud ;
